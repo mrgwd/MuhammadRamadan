fcc9244671b2fa824ab039059c5f5039c636e55a
muhammadramadan <medo44448888@gmail.com>
2024-09-10 11:58:41 +0300

    refactor: Update Footer component with new layout and social media links

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,28 +1,32 @@
 import { Envelope, Github, Linkedin, Twitter } from "@/components/common/icons";
 import Link from "next/link";
+import PageSize from "./pageSize";
 
 export default function Footer() {
   const date = new Date().getFullYear();
   return (
-    <footer className="flex items-center justify-between border-l border-light-secondary font-medium text-light-muted dark:border-dark-muted max-sm:flex-col">
+    <footer className="borderr-l flex items-start justify-between gap-y-2 border-light-secondary font-medium text-light-muted dark:border-dark-muted max-sm:flex-col">
       <div className="flex items-center gap-4">
         <p>Reach me out:</p>
         <div className="flex gap-2">
           <Link href="https://github.com/mrgwd" className="cursor-link">
-            <Github className="h-5 fill-light-muted hover:fill-primary-muted" />
+            <Github className="size-5 fill-light-muted transition-all hover:fill-primary-muted" />
           </Link>
           <Link href="https://twitter.com/_MuhammedR" className="cursor-link">
-            <Twitter className="h-5 fill-light-muted hover:fill-primary-muted" />
+            <Twitter className="size-5 fill-light-muted transition-all hover:fill-primary-muted" />
           </Link>
           <Link href="https://twitter.com/_MuhammedR" className="cursor-link">
-            <Linkedin className="h-5 fill-light-muted hover:fill-primary-muted" />
+            <Linkedin className="size-5 fill-light-muted transition-all hover:fill-primary-muted" />
           </Link>
           <Link href="mailto:mogdwd@gmail.com" className="cursor-link">
-            <Envelope className="h-5 fill-light-muted hover:fill-primary-muted" />
+            <Envelope className="size-5 fill-light-muted transition-all hover:fill-primary-muted" />
           </Link>
         </div>
       </div>
-      <p>&copy; mrgwd {date} • Got any feedback?</p>
+      <p>
+        &copy; mrgwd {date} • <PageSize />
+        KB
+      </p>
     </footer>
   );
 }
