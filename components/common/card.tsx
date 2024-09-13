93f0dffb8b6de50cb4a7b28303dff744d2556524
muhammadramadan <medo44448888@gmail.com>
2024-09-14 02:26:56 +0300

    feat: Simplify Card component by removing unused props and adjusting styles

--- components/common/card.tsx
+++ components/common/card.tsx
@@ -3,27 +3,16 @@ import { getReadTime } from "@/utils/getReadTime";
 import Link from "next/link";
 
 interface CardProps {
-  // variant?: "default" | "small" | "noImage";
   title: string;
-  id: string;
   content?: string;
   date: string;
   href: string;
-  // image?: string;
 }
-export default function Card({
-  // variant = "default",
-  title,
-  date,
-  content,
-  id,
-  href,
-  // readTime,
-}: CardProps) {
+export default function Card({ title, date, content, href }: CardProps) {
   const readTime = getReadTime(content as string);
   const formattedDate = formateDate(date);
   return (
-    <Link href={`/blog/${href}`} className="group flex items-center p-4">
+    <Link href={`/blog/${href}`} className="group flex items-center py-2">
       <div>
         <p className="relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:translate-y-1 after:bg-dark-muted after:opacity-0 after:transition-all after:content-[''] group-hover:after:translate-y-0 group-hover:after:opacity-100 dark:after:bg-light-muted sm:text-lg">
           {title}
