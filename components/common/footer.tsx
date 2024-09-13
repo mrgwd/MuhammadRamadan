c00fe62dd0bef59a8a6275849bb4fe747c3965da
muhammadramadan <medo44448888@gmail.com>
2024-09-14 02:25:35 +0300

    feat: Refactor Footer component for simple social links display

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -5,28 +5,31 @@ import PageSize from "./pageSize";
 export default function Footer() {
   const date = new Date().getFullYear();
   return (
-    <footer className="borderr-l flex items-start justify-between gap-y-2 border-light-secondary font-medium text-light-muted dark:border-dark-muted max-sm:flex-col">
-      <div className="flex items-center gap-4">
-        <p>Reach me out:</p>
-        <div className="flex gap-2">
-          <Link href="https://github.com/mrgwd" className="cursor-link">
-            <Github className="size-5 fill-light-muted transition-all hover:fill-primary-muted" />
-          </Link>
-          <Link href="https://twitter.com/_MuhammedR" className="cursor-link">
-            <Twitter className="size-5 fill-light-muted transition-all hover:fill-primary-muted" />
-          </Link>
-          <Link href="https://twitter.com/_MuhammedR" className="cursor-link">
-            <Linkedin className="size-5 fill-light-muted transition-all hover:fill-primary-muted" />
-          </Link>
-          <Link href="mailto:mogdwd@gmail.com" className="cursor-link">
-            <Envelope className="size-5 fill-light-muted transition-all hover:fill-primary-muted" />
-          </Link>
-        </div>
-      </div>
-      <p>
-        &copy; mrgwd {date} • <PageSize />
-        KB
-      </p>
+    <footer className="space-x-3 text-light-muted dark:border-dark-muted max-sm:flex-col">
+      <Link
+        href="https://twitter.com/_MuhammedR"
+        className="cursor-link transition-all dark:hover:text-light-primary"
+      >
+        @_MuhammedR
+      </Link>
+      <Link
+        href="https://linkedin.com/in/mrgwd"
+        className="cursor-link transition-all dark:hover:text-light-primary"
+      >
+        linkedin
+      </Link>
+      <Link
+        href="https://github.com/mrgwd"
+        className="cursor-link transition-all dark:hover:text-light-primary"
+      >
+        github
+      </Link>
+      <Link
+        href="mailto:mogdwd@gmail.com"
+        className="cursor-link transition-all dark:hover:text-light-primary"
+      >
+        email
+      </Link>
     </footer>
   );
 }
