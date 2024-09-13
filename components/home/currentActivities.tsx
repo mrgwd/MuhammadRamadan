c5ba1f99e36cfd85d3bf43557ecd5654e42ee236
muhammadramadan <medo44448888@gmail.com>
2024-09-14 01:03:27 +0300

    feat: Add CurrentActivities component to showcase personal activities

---
+++ components/home/currentActivities.tsx
@@ -0,0 +1,13 @@
+export default function CurrentActivities() {
+  return (
+    <section className="border border-t-0" data-fade-2>
+      <h2>Now</h2>
+      <ul className="mx-6 list-disc">
+        <li>Getting my YouTube channel ready to start making videos.</li>
+        <li>
+          Developing better habits to replace the old ones, to better myself.
+        </li>
+      </ul>
+    </section>
+  );
+}
