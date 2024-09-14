53d8cbfae05b6a64c4457034ef59882d3023f24f
muhammadramadan <medo44448888@gmail.com>
2024-09-14 14:13:50 +0300

    feat: Add utility function to format dates with customizable options

---
+++ utils/formateDate.ts
@@ -0,0 +1,12 @@
+export function formateDate(
+  date: string,
+  month: "2-digit" | "short" | "long" = "short",
+  day: "2-digit" | "numeric" = "numeric",
+  year: "2-digit" | "numeric" = "numeric",
+): string {
+  return new Date(date).toLocaleDateString("en-US", {
+    month,
+    day,
+    year,
+  });
+}
