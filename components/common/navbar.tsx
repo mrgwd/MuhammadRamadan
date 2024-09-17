f4092bbfff62e673fd389128a1af8d03148a601e
muhammadramadan <medo44448888@gmail.com>
2024-09-17 22:11:45 +0300

    style: Update Navbar component styles for minimal layout and spacing

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="borderr-r relative flex justify-between border-r-light-secondary px-3 py-10 text-lg text-dark-primary dark:border-r-dark-muted dark:text-light-primary sm:px-6">
+    <nav className="text-md mb-16 mt-12 flex justify-between text-dark-primary dark:text-light-primary">
       <ul className="flex items-center gap-4 sm:gap-6">
         <li>
           <Link
