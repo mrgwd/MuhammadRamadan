6e59853087b568d7bf2ce944659b6d280f11a8ed
muhammadramadan <medo44448888@gmail.com>
2024-09-17 22:10:20 +0300

    feat: Enhance MDXRenderer to include custom heading components with anchors

--- components/mdx/MDXRenderer.tsx
+++ components/mdx/MDXRenderer.tsx
@@ -2,11 +2,18 @@
 
 import { MDXRemote } from "next-mdx-remote";
 import type { MDXRemoteSerializeResult } from "next-mdx-remote";
+import { slugify } from "@/utils/slugify";
+import HeadingWithAnchor from "./headingWithAnchor";
 
 export default function MDXRenderer({
   source,
 }: {
   source: MDXRemoteSerializeResult;
 }) {
-  return <MDXRemote {...source} />;
+  const components = {
+    h2: (props: any) => (
+      <HeadingWithAnchor id={slugify(props.children)} {...props} />
+    ),
+  };
+  return <MDXRemote {...source} components={components} />;
 }
