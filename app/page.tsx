e0941d5253df8a9884f67527ab97694be2307516
muhammadramadan <medo44448888@gmail.com>
2024-09-18 22:58:36 +0300

    feat: Update Home component to use for more link cutomization

--- app/page.tsx
+++ app/page.tsx
@@ -3,36 +3,46 @@ import RecentPosts from "@/components/home/recentPosts";
 import React from "react";
 import HeroSection from "@/components/common/heroSection";
 import { PaperClip, Twitter, Github } from "@/components/common/icons";
-import SocialLink from "@/components/home/socialLink";
+import Link from "next/link";
+import ExternalLink from "@/components/home/externalLink";
 
 export default function Home(): JSX.Element {
   return (
-    <main className="text-dark-primary dark:text-light-primary">
-      <HeroSection
-        title="hey, I'm Muhammad 👋"
-        description="I am a Frontend Engineer, I work with the React ecosystem. I design
-          and develop responsive and user-friendly user interfaces. I share my
-          thoughts often to teach people about web development, JS, TS, React
-          and Next.js."
-      >
+    <main className="text-dark-primary dark:text-light-secondary">
+      <HeroSection title="hey, I'm Muhammad 👋">
+        <p>
+          I am a Frontend Engineer, I{" "}
+          <Link href="/work" className="link">
+            work
+          </Link>{" "}
+          with the React ecosystem. I design and develop responsive and
+          user-friendly user interfaces. I{" "}
+          <Link href="/blog" className="link">
+            share
+          </Link>{" "}
+          my thoughts often to teach people about web development, JS, TS, React
+          and Next.js.
+        </p>
         <div className="flex flex-wrap gap-x-8 gap-y-2 text-light-muted">
-          <SocialLink href="./assets/resume.pdf" handle="Resume">
-            <PaperClip className="size-5 stroke-light-muted grayscale group-hover:stroke-dark-muted group-hover:grayscale-0 dark:group-hover:stroke-light-primary" />
-          </SocialLink>
+          <ExternalLink href="./assets/resume.pdf" handle="Resume">
+            <PaperClip className="size-5 stroke-dark-muted group-hover:stroke-dark-primary dark:stroke-light-muted dark:group-hover:stroke-light-primary" />
+          </ExternalLink>
 
-          <SocialLink
+          <ExternalLink
             href="https://www.twitter.com/_MuhammedR"
             handle="@_MuhammedR"
           >
             <Twitter className="size-5 fill-light-muted group-hover:fill-[#1da1f2]" />
-          </SocialLink>
+          </ExternalLink>
 
-          <SocialLink href="https://www.github.com/mrgwd" handle="mrgwd">
-            <Github className="size-5 fill-light-muted group-hover:fill-dark-muted dark:group-hover:fill-light-primary" />
-          </SocialLink>
+          <ExternalLink href="https://www.github.com/mrgwd" handle="mrgwd">
+            <Github className="size-5 fill-dark-muted group-hover:fill-dark-primary dark:fill-light-muted dark:group-hover:fill-light-primary" />
+          </ExternalLink>
         </div>
       </HeroSection>
+      {/* <div className="divider"></div> */}
       <CurrentActivities />
+      {/* <div className="divider"></div> */}
       <RecentPosts />
     </main>
   );
