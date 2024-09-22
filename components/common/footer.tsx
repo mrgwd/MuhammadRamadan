881280704d7a0cd6dfa8b4835913b6ff78e89071
muhammadramadan <medo44448888@gmail.com>
2024-09-22 10:18:08 +0300

    feat: Refactor Footer component to use custom reusable ExternalLink component

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,35 +1,15 @@
-import { Envelope, Github, Linkedin, Twitter } from "@/components/common/icons";
-import Link from "next/link";
-import PageSize from "./pageSize";
+import ExternalLink from "../home/externalLink";
 
 export default function Footer() {
-  const date = new Date().getFullYear();
   return (
-    <footer className="space-x-3 text-light-muted dark:border-dark-muted max-sm:flex-col">
-      <Link
+    <footer className="mb-10 mt-16 flex space-x-3 text-dark-muted hover:text-dark-primary dark:text-light-muted dark:hover:text-light-primary">
+      <ExternalLink
         href="https://twitter.com/_MuhammedR"
-        className="cursor-link transition-all dark:hover:text-light-primary"
-      >
-        @_MuhammedR
-      </Link>
-      <Link
-        href="https://linkedin.com/in/mrgwd"
-        className="cursor-link transition-all dark:hover:text-light-primary"
-      >
-        linkedin
-      </Link>
-      <Link
-        href="https://github.com/mrgwd"
-        className="cursor-link transition-all dark:hover:text-light-primary"
-      >
-        github
-      </Link>
-      <Link
-        href="mailto:mogdwd@gmail.com"
-        className="cursor-link transition-all dark:hover:text-light-primary"
-      >
-        email
-      </Link>
+        handle="@_MuhammedR"
+      />
+      <ExternalLink href="https://linkedin.com/in/mrgwd" handle="linkedin" />
+      <ExternalLink href="https://github.com/mrgwd" handle="github" />
+      <ExternalLink href="mailto:mogdwd@gmail.com" handle="email" />
     </footer>
   );
 }
