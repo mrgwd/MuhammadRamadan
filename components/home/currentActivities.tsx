c4e802d81707eea6a3b889b43205df4c73e02510
muhammadramadan <medo44448888@gmail.com>
2024-09-22 10:52:38 +0300

    feat: Update CurrentActivities component with new content and styling

--- components/home/currentActivities.tsx
+++ components/home/currentActivities.tsx
@@ -1,12 +1,20 @@
+import Link from "next/link";
+
 export default function CurrentActivities() {
   return (
     <section data-fade-2>
-      <h2 className="font-medium text-light-primary">Now</h2>
+      <h2 className="font-medium text-dark-primary dark:text-light-primary">
+        Now
+      </h2>
       <p className="">
-        Getting my YouTube channel ready to start making videos and developing
-        better habits to replace the old ones, to better myself.
+        Working on some{" "}
+        <Link href="https://github.com/mrgwd" className="link cursor-link">
+          open-source projects
+        </Link>
+        , getting my YouTube channel ready to start making videos, and seeking a
+        new challenge as a frontend developer.
       </p>
-      <hr />
+      {/* <hr /> */}
     </section>
   );
 }
