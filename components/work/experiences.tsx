16b84f60b14d6f4bdb0565dd71ba56a43823e1c4
muhammadramadan <medo44448888@gmail.com>
2024-09-22 11:17:42 +0300

    feat: Refactor Experiences component for improved styling and content clarity

--- components/work/experiences.tsx
+++ components/work/experiences.tsx
@@ -2,11 +2,8 @@ import Experience from "./experience";
 
 export default function Experiences() {
   return (
-    <section className="space-y-0 p-0">
-      <article
-        className="border border-t-0 border-light-secondary opacity-0 animation-delay-300 dark:border-dark-muted"
-        data-fade-2
-      >
+    <section className="text-dark-secondary dark:text-light-secondary">
+      <article className="space-y-4" data-fade-2>
         <Experience
           name="ATHR"
           role="UI Developer"
@@ -14,10 +11,10 @@ export default function Experiences() {
           dateTo="Jun, 2024"
           summary="I joined ATHR as a UI Developer. I was responsible for designing, building and maintaining the user interface of the web application."
           achievements={[
-            "Built responsive and user-friendly user interfaces.",
-            "Implemented new features and fixed bugs.",
-            "Optimized websites performance by minimizing load times and reducing page weight.",
-            "Worked closely with back-end developers to integrate front-end and back-end functionality.",
+            "Designed and built responsive, user-friendly interfaces.",
+            "Developed reusable components using React, TypeScript, and Next.js.",
+            "Optimized website performance by reducing load times and page weight.",
+            "Collaborated with back-end developers to integrate front-end and back-end functionality.",
           ]}
           closing="During my time at ATHR, I gained my first production experience using React, learned how to work in a team and how to communicate effectively with other team members."
         />
