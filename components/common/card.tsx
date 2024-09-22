238c21763ae9f682f931271ef46b4dbf80c5cf66
muhammadramadan <medo44448888@gmail.com>
2024-09-22 10:52:00 +0300

    feat: Fix formatDate import and simplify Card component structure

--- components/common/card.tsx
+++ components/common/card.tsx
@@ -1,24 +1,22 @@
-import { formateDate } from "@/utils/formateDate";
+import { formatDate } from "@/utils/formatDate";
 import { getReadTime } from "@/utils/getReadTime";
 import Link from "next/link";
 
 interface CardProps {
   title: string;
-  content?: string;
   date: string;
   href: string;
 }
-export default function Card({ title, date, content, href }: CardProps) {
-  const readTime = getReadTime(content as string);
-  const formattedDate = formateDate(date);
+export default function Card({ title, date, href }: CardProps) {
+  const formattedDate = formatDate(date);
   return (
-    <Link href={`/blog/${href}`} className="group flex items-center py-2">
-      <div>
-        <p className="relative after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:translate-y-1 after:bg-dark-muted after:opacity-0 after:transition-all after:content-[''] group-hover:after:translate-y-0 group-hover:after:opacity-100 dark:after:bg-light-muted sm:text-lg">
+    <Link href={`/blog/${href}`} className="group">
+      <div className="my-2">
+        <p className="decoration-transparent underline-offset-2 transition-all duration-300 group-hover:underline group-hover:decoration-inherit">
           {title}
         </p>
-        <p className="text-dark-muted dark:text-light-muted">
-          <span>{formattedDate}</span> {/* • <span> {readTime}</span>*/}
+        <p className="flex justify-between text-dark-muted dark:text-light-muted">
+          <span>{formattedDate}</span>
         </p>
       </div>
     </Link>
