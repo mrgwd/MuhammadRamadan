a43daac2a920a60519f2e4f065b3e63c92caf8fc
muhammadramadan <medo44448888@gmail.com>
2024-09-24 09:06:08 +0300

    feat: Implement dynamic metadata generation for blog articles to enhance SEO

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,13 +2,33 @@ import { getMDXComponent } from "mdx-bundler/client";
 import { getMdxSource } from "@/lib/mdx";
 import { notFound } from "next/navigation";
 import Header from "@/components/blog/header";
-// import HeadingWithAnchor from "@/components/mdx/headingWithAnchor";
+import { Metadata } from "next";
 
-export default async function ArticlePage({
+export async function generateMetadata({
   params,
 }: {
   params: { slug: string };
-}) {
+}): Promise<Metadata> {
+  const { slug } = params;
+  const post = await getMdxSource(slug);
+
+  if (!post) {
+    return {
+      title: "Not Found",
+      description: "The article you are looking for does not exist.",
+    };
+  }
+
+  const { frontMatter } = post;
+
+  return {
+    title: `${frontMatter.title}`,
+    description: frontMatter.description.slice(0, 160),
+    keywords: frontMatter.tags,
+  };
+}
+
+export default async function Post({ params }: { params: { slug: string } }) {
   const { slug } = params;
   const post = await getMdxSource(slug);
 
@@ -19,17 +39,12 @@ export default async function ArticlePage({
   const { code, frontMatter } = post;
   const Component = getMDXComponent(code);
 
-  // const components = {
-  //   h2: (props: any) => <HeadingWithAnchor id={props.children} {...props} />,
-  // };
-
   return (
     <main className="article">
       <Header title={frontMatter.title} createdAt={frontMatter.createdAt} />
       <article data-fade-2>
-        <Component /*components={{ components }}*/ />{" "}
+        <Component />
       </article>
-      {/* Render MDX */}
     </main>
   );
 }
